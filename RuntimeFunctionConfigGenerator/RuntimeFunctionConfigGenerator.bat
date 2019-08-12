e69962b34f3a42b89355dd718e48eb59f4776d2f
文煌 <wwh1004@hotmail.com>
2019-08-12 12:36:58 +0800

    update RuntimeFunctionConfigGenerator.bat

--- RuntimeFunctionConfigGenerator/RuntimeFunctionConfigGenerator.bat
+++ RuntimeFunctionConfigGenerator/RuntimeFunctionConfigGenerator.bat
@@ -1,4 +1,4 @@
 @echo off
-Tool.Loader.CLR20.x86.exe RuntimeFunctionConfigGenerator.dll -d YOUR_SYMBOLS_PATH
-Tool.Loader.CLR40.x86.exe RuntimeFunctionConfigGenerator.dll -d YOUR_SYMBOLS_PATH
+Tool.Loader.CLR20.x86.exe RuntimeFunctionConfigGenerator.dll
+Tool.Loader.CLR40.x86.exe RuntimeFunctionConfigGenerator.dll
 pause
